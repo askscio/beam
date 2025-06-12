8f0b2a69619dfafea1bff0f6936f65853f8a149b
Xinyu Liu <xinyu.liu@glean.com>
2025-06-12 10:02:32 -0700

    Update the cap to be 5MB

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/LargeRecordFilterFunction.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/LargeRecordFilterFunction.java
@@ -8,11 +8,15 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * [Glean]
+ * FilterFunction that filters out large records based on a size threshold.
+ */
 public class LargeRecordFilterFunction<K, V> implements
     FilterFunction<WindowedValue<KV<K, V>>> {
   private static final Logger LOG =
       LoggerFactory.getLogger(LargeRecordFilterFunction.class);
-  private static final long MAX_RECORD_SIZE =  1000000; // 1 MB
+  private static final long MAX_RECORD_SIZE =  5000000; // 5 MB
 
   @Override
   public boolean filter(WindowedValue<KV<K, V>> windowedValue) throws Exception {
@@ -25,15 +29,17 @@ public class LargeRecordFilterFunction<K, V> implements
     return true;
   }
 
+  /**
+   * Calculate the size of an object in bytes.
+   * This is a simplified version for objects used in portability.
+   */
   private static <T> long getObjectSize(T o) {
-    // This method should return the size of the object in bytes.
-    // Implement this based on your requirements or use a library that can calculate object size.
     if (o instanceof byte[]) {
       return ((byte[]) o).length;
     } else if(o instanceof List) {
       return ((List<?>) o).stream().mapToLong(LargeRecordFilterFunction::getObjectSize).sum();
     } else {
-      return 0; // Assume an average size for non-byte array or list objects.
+      return 0; // for other types, we don't calculate size
     }
   }
 }
